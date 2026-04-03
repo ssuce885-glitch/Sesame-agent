72029dc775a93715e1e061493e0c11a56686c0d4
ssuce885-glitch <ssuce885@gmail.com>
2026-04-03 22:00:03 +0800

    fix: harden fake streaming client

--- internal/model/fake.go
+++ internal/model/fake.go
@@ -35,22 +35,49 @@ func NewFakeStreaming(streams [][]StreamEvent) *FakeStreaming {
 	return &FakeStreaming{streams: streams}
 }
 
-func (f *FakeStreaming) Stream(_ context.Context, _ Request) (<-chan StreamEvent, <-chan error) {
-	events := make(chan StreamEvent)
-	errs := make(chan error, 1)
+func (f *FakeStreaming) Stream(ctx context.Context, _ Request) (<-chan StreamEvent, <-chan error) {
+	var (
+		batch []StreamEvent
+		err   error
+	)
 
-	var batch []StreamEvent
-	if f.index < len(f.streams) {
+	if ctx != nil && ctx.Err() != nil {
+		err = ctx.Err()
+	} else if f.index >= len(f.streams) {
+		err = errNoMoreResponses
+	} else {
 		batch = f.streams[f.index]
 		f.index++
+	}
+
+	events := make(chan StreamEvent, len(batch))
+	errs := make(chan error, 1)
+	var done <-chan struct{}
+	if ctx != nil {
+		done = ctx.Done()
 	}
 
 	go func() {
 		defer close(events)
 		defer close(errs)
 
+		if err != nil {
+			errs <- err
+			return
+		}
+
 		for _, event := range batch {
-			events <- event
+			select {
+			case <-done:
+				errs <- ctx.Err()
+				return
+			case events <- event:
+			}
+		}
+
+		if ctx != nil && ctx.Err() != nil {
+			errs <- ctx.Err()
+			return
 		}
 
 		errs <- nil
